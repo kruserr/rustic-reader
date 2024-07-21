1667e4cc3221a0e199aa97c10f1834d4178fb182
kruserr <46799551+kruserr@users.noreply.github.com>
2024-07-21 11:37:50 +0000

    ci: publish cli-epub-to-text

--- .publish.sh
+++ .publish.sh
@@ -2,7 +2,8 @@
 
 set -Eeuo pipefail
 
-cargo publish -p cli-justify
 cargo publish -p cli-pdf-to-text
+cargo publish -p cli-epub-to-text
+cargo publish -p cli-justify
 cargo publish -p cli-text-reader
 cargo publish -p rustic-reader
