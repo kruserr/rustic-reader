3e75e7b457a7622e8df31c300f9acac569cdcc6a
kruserr <46799551+kruserr@users.noreply.github.com>
2024-09-22 10:50:13 +0000

    ci: add redirect-stderr

--- .publish.sh
+++ .publish.sh
@@ -6,4 +6,5 @@ cargo publish -p cli-pdf-to-text
 cargo publish -p cli-epub-to-text
 cargo publish -p cli-justify
 cargo publish -p cli-text-reader
+cargo publish -p redirect-stderr
 cargo publish -p rustic-reader
