02519642c5a677342cb488411fa0576248f57baa
kruserr <46799551+kruserr@users.noreply.github.com>
2024-12-03 10:56:17 +0000

    feat: basic file upload

--- hygg-server/src/main.rs
+++ hygg-server/src/main.rs
@@ -1,10 +1,9 @@
-use std::error::Error;
-use std::fs::{File, OpenOptions};
-use std::io::Write;
-use std::path::Path;
-use std::thread::sleep;
+use bytes::BufMut;
+use futures::{StreamExt, TryStreamExt};
 use tracing_subscriber::fmt::format::FmtSpan;
-use warp::Filter;
+use std::convert::Infallible;
+use uuid::Uuid;
+use warp::{http::StatusCode, multipart::FormData, Filter, Rejection, Reply};
 
 pub fn mkdir(
   input: &str,
@@ -15,21 +14,91 @@ pub fn mkdir(
   Ok("".into())
 }
 
-pub fn create_server_http(port: u16) -> Result<(), Box<dyn Error>> {
-  tracing_subscriber::fmt().with_span_events(FmtSpan::CLOSE).init();
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().with_span_events(FmtSpan::CLOSE).init();
 
-  let uploads_dir = "hygg-server-uploads";
-  mkdir(uploads_dir);
+    let uploads_dir = "hygg-server-uploads";
+    mkdir(uploads_dir);
 
-  let rt = tokio::runtime::Runtime::new()?;
-  rt.block_on(
-    warp::serve(warp::fs::dir(uploads_dir).with(warp::trace::request()))
-      .run(([0, 0, 0, 0], port)),
-  );
+    let upload_route = warp::path("upload")
+        .and(warp::post())
+        .and(warp::multipart::form().max_length(50_000_000))
+        .and_then(move |x| upload(x, &uploads_dir));
 
-  Ok(())
+    let download_route = warp::fs::dir(uploads_dir);
+
+    let router = upload_route.or(download_route).recover(handle_rejection);
+    warp::serve(router.with(warp::trace::request())).run(([0, 0, 0, 0], 3030)).await;
 }
 
-pub fn main() {
-  create_server_http(3030);
+async fn upload(form: FormData, uploads_dir: &str) -> Result<impl Reply, Rejection> {
+    let mut parts = form.into_stream();
+    while let Some(Ok(p)) = parts.next().await {
+        if p.name() == "file" {
+            let content_type = p.content_type();
+            let file_ending;
+
+            println!("{content_type:?}");
+
+            match content_type {
+                Some(file_type) => match file_type {
+                    "application/pdf" => {
+                        file_ending = "pdf";
+                    }
+                    "application/epub+zip" => {
+                        file_ending = "epub";
+                    }
+                    "text/html" => {
+                        file_ending = "html";
+                    }
+                    v => {
+                        eprintln!("invalid file type found: {}", v);
+                        return Err(warp::reject::reject());
+                    }
+                },
+                None => {
+                    eprintln!("file type could not be determined");
+                    return Err(warp::reject::reject());
+                }
+            }
+
+            let value = p
+                .stream()
+                .try_fold(Vec::new(), |mut vec, data| {
+                    vec.put(data);
+                    async move { Ok(vec) }
+                })
+                .await
+                .map_err(|e| {
+                    eprintln!("reading file error: {}", e);
+                    warp::reject::reject()
+                })?;
+
+            let file_name = format!("./{uploads_dir}/{}.{}", Uuid::new_v4(), file_ending);
+            tokio::fs::write(&file_name, value).await.map_err(|e| {
+                eprint!("error writing file: {}", e);
+                warp::reject::reject()
+            })?;
+            println!("created file: {}", file_name);
+        }
+    }
+
+    Ok("success")
+}
+
+async fn handle_rejection(err: Rejection) -> std::result::Result<impl Reply, Infallible> {
+    let (code, message) = if err.is_not_found() {
+        (StatusCode::NOT_FOUND, "Not Found".to_string())
+    } else if err.find::<warp::reject::PayloadTooLarge>().is_some() {
+        (StatusCode::BAD_REQUEST, "Payload too large".to_string())
+    } else {
+        eprintln!("unhandled error: {:?}", err);
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Internal Server Error".to_string(),
+        )
+    };
+
+    Ok(warp::reply::with_status(message, code))
 }
---
+++ test-server.sh
@@ -0,0 +1,3 @@
+curl --location --request POST 'http://localhost:3030/upload' \
+--header 'Content-Type: multipart/form-data' \
+--form 'file=@./test-data/pdf/pdfreference1.7old-1-50.pdf'
