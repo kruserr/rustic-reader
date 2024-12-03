427fcac4c5652979a01c223efd79c4b228e1368b
kruserr <46799551+kruserr@users.noreply.github.com>
2024-12-03 10:04:39 +0000

    chore: init hygg-server

--- hygg-server/src/main.rs
+++ hygg-server/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-  println!("hygg-server");
+use std::error::Error;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::path::Path;
+use std::thread::sleep;
+use tracing_subscriber::fmt::format::FmtSpan;
+use warp::Filter;
+
+pub fn create_server_http(port: u16) -> Result<(), Box<dyn Error>> {
+  tracing_subscriber::fmt().with_span_events(FmtSpan::CLOSE).init();
+
+  let rt = tokio::runtime::Runtime::new()?;
+  rt.block_on(
+    warp::serve(warp::fs::dir(".").with(warp::trace::request()))
+      .run(([0, 0, 0, 0], port)),
+  );
+
+  Ok(())
+}
+
+pub fn main() {
+  create_server_http(3030);
 }
