83f4a2ac4ed7959f5ee85a288cd79fb916be746b
kruserr <46799551+kruserr@users.noreply.github.com>
2024-12-03 10:11:07 +0000

    feat: basic file download

--- hygg-server/src/main.rs
+++ hygg-server/src/main.rs
@@ -6,12 +6,24 @@ use std::thread::sleep;
 use tracing_subscriber::fmt::format::FmtSpan;
 use warp::Filter;
 
+pub fn mkdir(
+  input: &str,
+) -> Result<String, Box<dyn std::error::Error + Send>> {
+  let path = std::path::Path::new(input);
+  std::fs::create_dir_all(path)
+    .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send>)?;
+  Ok("".into())
+}
+
 pub fn create_server_http(port: u16) -> Result<(), Box<dyn Error>> {
   tracing_subscriber::fmt().with_span_events(FmtSpan::CLOSE).init();
 
+  let uploads_dir = "hygg-server-uploads";
+  mkdir(uploads_dir);
+
   let rt = tokio::runtime::Runtime::new()?;
   rt.block_on(
-    warp::serve(warp::fs::dir(".").with(warp::trace::request()))
+    warp::serve(warp::fs::dir(uploads_dir).with(warp::trace::request()))
       .run(([0, 0, 0, 0], port)),
   );
 
